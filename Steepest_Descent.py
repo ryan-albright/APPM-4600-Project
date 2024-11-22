31fe40474b14825578a92edb61dd8832045495c1
ryan-albright <ryan.albright@colorado.edu>
2024-11-21 18:02:42 -0700

    v8

--- Steepest_Descent.py
+++ Steepest_Descent.py
@@ -4,40 +4,15 @@ from numpy.linalg import norm
 import time
 
 
-def driver ():
+def driver (exp_type):
     # Convergence Experiments
-    print(f'Please see the below experiment to test convergence of the steepest descent method')
-    K = 100
-    rate_n = []
+    if exp_type == 'constant n':
+        print(f'Please see the below experiment to test convergence of the steepest descent method')
+        K = 100
+        rate_n = []
 
-    # Experiment for changing n and keeping K constant
-    for n in [10,100,1000]:
-        # create a symmetric positive definite matrix
-        [Q, R] = np.linalg.qr(np.random.randn(n,n))
-        D = np.diag(np.linspace(1,K,n))
-    
-        A = Q @ D @ Q.T
-        b = np.random.randn(n,1)
-        b = b / norm(b)
-
-        [x, iterates, it_count] = SD(A, b, 1e-8, 5000)
-
-        diffs = iterates - x.T
-        err = np.array([norm(diff) for diff in diffs])
-        rate_n.append(np.average(err[1:] / err[:-1]))
-        plt.semilogy(err)
-    
-    plt.ylabel('en+1 - en')
-    plt.xlabel('Iteration')
-    # Caption Iteration of Steepest Descent Method on an A matrix of size n x n with condition number K = 20
-    plt.legend(['n = 10', 'n = 100', 'n = 1000'])
-    plt.show()
-    print(f'The order of convergence is linear for n = [10, 100, 1000] at rate {rate_n}')
-
-    # Experiment for changing K and keeping n constant
-    for n in [10,100,1000]:
-        plt.figure()
-        for K in [10, 100, 1000, 10000]:
+        # Experiment for changing n and keeping K constant
+        for n in [10,100,1000]:
             # create a symmetric positive definite matrix
             [Q, R] = np.linalg.qr(np.random.randn(n,n))
             D = np.diag(np.linspace(1,K,n))
@@ -46,27 +21,60 @@ def driver ():
             b = np.random.randn(n,1)
             b = b / norm(b)
 
-            [x, iterates, it_count] = SD(A, b, 1e-8, 5000, verb = False)
+            [x, iterates, it_count] = SD(A, b, 1e-8, 5000)
 
             diffs = iterates - x.T
             err = np.array([norm(diff) for diff in diffs])
+            rate_n.append(np.average(err[1:] / err[:-1]))
             plt.semilogy(err)
+        
         plt.ylabel('en+1 - en')
         plt.xlabel('Iteration')
-        plt.legend(['K = 10', 'K = 100', 'K = 1000', 'K = 10000'])
-    plt.show()
+        # Caption Iteration of Steepest Descent Method on an A matrix of size n x n with condition number K = 20
+        plt.legend(['n = 10', 'n = 100', 'n = 1000'])
+        plt.show()
+        print(f'The order of convergence is linear for n = [10, 100, 1000] at rate {rate_n}')
 
+    # Experiment for changing K and keeping n constant
+    elif exp_type == 'constant_k':
+        for n in [10,100,1000]:
+            plt.figure()
+            for K in [10, 100, 1000, 10000]:
+                # create a symmetric positive definite matrix
+                [Q, R] = np.linalg.qr(np.random.randn(n,n))
+                D = np.diag(np.linspace(1,K,n))
+            
+                A = Q @ D @ Q.T
+                b = np.random.randn(n,1)
+                b = b / norm(b)
 
+                [x, iterates, it_count] = SD(A, b, 1e-8, 5000, verb = False)
 
+                diffs = iterates - x.T
+                err = np.array([norm(diff) for diff in diffs])
+                plt.semilogy(err)
+            plt.ylabel('en+1 - en')
+            plt.xlabel('Iteration')
+            plt.legend(['K = 10', 'K = 100', 'K = 1000', 'K = 10000'])
+        plt.show()
 
     # Time experiments
-    # print(f'Please see below the experiments performed to test run time with n = {n} and kappa = {K}')
-    # t_0 = time.time()
-    # for j in range(100):
-    #     [x, iterates, it_count] = SD(A, b, 1e-8, 5000, verb = False)
-    # t_1 = time.time()
-    # print(f'Average time: {(t_1 - t_0)/100} seconds')
-    # print(f'Number of iterations: {it_count}')
+    elif exp_type == 'time':
+        n, K = 10, 100
+        print(f'Please see below the experiments performed to test run time with n = {n} and kappa = {K}')
+        [Q, R] = np.linalg.qr(np.random.randn(n,n))
+        D = np.diag(np.linspace(1,K,n))
+    
+        A = Q @ D @ Q.T
+        b = np.random.randn(n,1)
+        b = b / norm(b)
+
+        t_0 = time.time()
+        for j in range(100):
+            [x, iterates, it_count] = SD(A, b, 1e-8, 5000, verb = False)
+        t_1 = time.time()
+        print(f'Average time: {(t_1 - t_0)/100} seconds')
+        print(f'Number of iterations: {it_count}')
 
 def SD (A, b, tol, nmax, verb = True):
     '''
@@ -102,4 +110,4 @@ def SD (A, b, tol, nmax, verb = True):
         print('Maximum Number of iterations exceeded')
     return [0, iterates[:i,:], i] 
 
-driver()
+driver('time') # choose experiment type: 'constant_n', 'constant_k', or 'timing'
