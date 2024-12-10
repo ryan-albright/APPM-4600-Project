d3263a92abf5c8709248f5e231efafb85bc1166d
ryan-albright <ryan.albright@colorado.edu>
2024-12-09 17:06:04 -0700

    v2

--- PDE.py
+++ PDE.py
@@ -15,8 +15,8 @@ def poisson(dimension, n):
         b = np.pi
 
         # Dirichlet Boundary Conditions
-        u_0 = 5
-        u_n = 2
+        u_0 = 3
+        u_n = -2
 
         # u''(x) = f(x)
         f = lambda x: -np.cos(x) # Change print statement and graph title when you change this
@@ -46,12 +46,51 @@ def poisson(dimension, n):
         plt.plot(x,y)
         plt.title(f'Plot of u(x) solving the 1D Possions equation for f(x) = -cos(x)')
 
-        # Plot of the analytical solution to the PDE
-        # x1 = np.linspace(a,b)
-        # plt.plot(x, np.cos(x))
+        
+        plt.show()
+    
+    elif dimension == 1:
+        n_list = n
+        for n in n_list:
+            # # Define initial problem # #
+        
+            # Interval
+            a = 0
+            b = np.pi
 
-        # plt.legend(['Numerical', 'Actual'])
+            # Dirichlet Boundary Conditions
+            u_0 = 1
+            u_n = 1
 
+            # u''(x) = f(x)
+            f = lambda x: -np.cos(x) # Change print statement and graph title when you change this
+
+            print(f"Here is the numerical solution for -u''(x) = -cos(x) wih Dirichlet boundary conditions u({a}) = {u_0} and u({b}) = {u_n}")
+
+            # Define b vector (values of f)
+            x = np.linspace(a,b,n)
+            h = x[1] - x[0]
+
+            b = f(x)
+
+            # Adding Dirichlet Conditions to the b vector
+            b[0] = b[0] - u_0 / h**2
+            b[-1] = b[-1] - u_n / h**2
+
+            # Creating A matrix
+            A1 = np.diag([2]*n)
+            A2 = np.diag([-1]*(n-1), 1)
+            A3 = np.diag([-1]*(n-1),-1)
+
+            A = (-1/h**2)*(A1 + A2 + A3)
+
+            [y, iterates, it_count]  = CG(A,b,10**-8, 3000, True)
+
+            # Plot of the numerical solution to the PDE
+            plt.plot(x,y)
+        
+        plt.title(f'Plot of u(x) solving the 1D Possions equation for f(x) = -cos(x)')
+        
         plt.show()
     
     elif dimension == 2:
@@ -61,43 +100,21 @@ def poisson(dimension, n):
         a = -np.pi / np.sqrt(2)
         b = np.pi / np.sqrt(2)
 
-        # Dirichlet Boundary Conditions
-        j = 0
+        a, b = -5, 5
 
-        print("Here is the numerical solution for 2D Poisson on a square wih Dirichlet boundary conditions u(x,y) = {j} on the boundary")
+        # Dirichlet Boundary Conditions
+        gamma = lambda x,y: x*y*0 + 1
+
+        print(f"Here is the numerical solution for 2D Poisson on a square wih Dirichlet boundary conditions u(x,y) = 1 on the boundary")
 
         # Defining b vector
-        x1 = np.linspace(a,b,n)
-        x2 = np.linspace(a,b,n)
+        x1 = np.linspace(a,b,n+2)
+        x2 = np.linspace(a,b,n+2)
 
-        h = (b - a) / (n + 1)
+        h = (b - a) / (n - 1)
 
-        g1, g2 = np.meshgrid(x1,x2)
+        x1_grid, x2_grid = np.meshgrid(x1,x2)
 
-        # Coordinate Rotation
-        alpha = 1 / np.sqrt(2)
-        
-        g1_new = g1 * alpha - g2 * alpha
-        g2_new = g1 * alpha + g2 * alpha
-        
-        f = func_2d(g1_new,g2_new)
-        
-        b = f.flatten()
-
-        # Adding Dirichlet Conditions to the b vector
-
-        # Make first n+1 entries zero
-        b[:n+1] = np.array([j]*(n+1))
-
-        # Make middle entries 0 where needed
-        for i in range(n**2):
-            if i % n == 0:
-                b[i-1] = j
-                b[i] = j
-
-        # Make last n+1 entries zero
-        b[-n:] = np.array([j]*(n))
-                
         # Creating A matrix
         size = n**2
         A1 = np.diag([4]*size)
@@ -106,33 +123,69 @@ def poisson(dimension, n):
         A4 = np.diag([-1]*(n**2 - n),n)
         A5 = np.diag([-1]*(n**2 - n),-n)
 
-        # A = (-1/h**2)
-
         A = (-1/h**2)*(A1 + A2 + A3 +A4 + A5)
 
-        # Modifying top and bottom of A for boundary conditions
-        A[:n+1,:] = np.hstack((np.eye(n+1), np.zeros((n + 1, n**2 - n - 1))))
+        for i in range(n**2):
+            for j in range(n**2):
+                if i % n == 0 and j % n == 0:
+                    A[i,j-1] = 0
+                    A[i-1,j] = 0
 
-        A[-n:,:] = np.hstack(( np.zeros((n, n**2 - n)),np.eye(n)))
+        # Creating b vector
+        b = np.empty((n,n))
 
-        for i in range(n+1,n**2):
-            if i % n == 0:
-                row_i = np.zeros(n**2)
-                row_i[i] = 1
-                A[i,:] = row_i
-                row_i1 = np.zeros(n**2)
-                row_i1[i-1] = 1
-                A[i-1,:] = row_i1
-        
+        for i in range(1,n+1):
+            for j in range(1,n+1):
+                xhm, yhm = x1[i-1], x2[i-1]
+                x, y = x1[i], x2[j]
+                xhp, yhp = x1[i+1], x2[i+1]
+                # top left corner
+                if i == 1 and j == 1:
+                    b[i-1,j-1] = func_2d(x,y) - gamma(x,yhp) / h**2 - gamma(xhm,y) / h**2
+                # top side
+                elif j == 1 and i != 1 and i != n:
+                    b[i-1,j-1] = func_2d(x,y) - gamma(x,yhp) / h**2
+                # top right corner
+                elif i == n and j == 1:
+                    b[i-1,j-1] = func_2d(x,y) - gamma(x,yhp) / h**2 - gamma(xhp,y) / h**2
+                # left side
+                elif i == 1 and j != 1 and j != n:
+                    b[i-1,j-1] = func_2d(x,y) - gamma(xhm,y) / h**2
+                # right side
+                elif i == n and j != 1 and j != n:
+                    b[i-1,j-1] = func_2d(x,y) - gamma(xhp,y) / h**2
+                # bottom left corner
+                elif i == 1 and j == n:
+                    b[i-1,j-1] = func_2d(x,y) - gamma(xhm,y) / h**2 - gamma(x,yhm) / h**2
+                # bottom side
+                elif j == n and i != 1 and i != n:
+                    b[i-1,j-1] = func_2d(x,y) - gamma(x,yhm) / h**2
+                # bottom right corner
+                elif i == n and j == n:
+                    b[i-1,j-1] = func_2d(x,y) - gamma(x,yhm) / h**2 - gamma(xhp,y) / h**2
+                # interior points
+                else:
+                    b[i-1,j-1] = func_2d(x,y)
+
+        b = b.flatten()
+
+        # creating grid of points to plot
+        # We basically need to add the boundary points to the outside of the matrix
+        bv_mat = np.zeros((n+2, n+2))
+                    
         [y, iterates, it_count]  = CG(A,b,10**-8, 5000, True)
-        
+                
         fig = plt.figure()
         ax = plt.axes(projection='3d')
-        
-        z = y.reshape(n,n)
-        surf = ax.plot_surface(g1, g2, z, cmap='viridis')
 
+        z = y.reshape(n,n)
+        # We basically need to add the boundary points to the outside of the matrix
+        y = gamma(x1_grid, x2_grid)
+        y[1:n+1,1:n+1] = z
+
+        surf = ax.plot_surface(x1_grid, x2_grid, y, cmap='viridis')
         plt.show()
+
 
 def func_2d(x, y):
         return -np.sin(x + np.pi/2) - np.cos(y)
@@ -195,7 +248,8 @@ def CG(A, b, tol, nmax, verb=True):
         print('Maximum Number of iterations exceeded')
     return x, iterates[:i+1, :], i+1#, x_star        
 
-poisson(1, 1000)
+# poisson(1, [10,100,1000])
+poisson(2,50)
 
 
         
